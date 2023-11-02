66b49490a0995cdb7f4c331e5b580b6444cf302e
Jessica Nelson <jessica.nelson60@gmail.com>
2023-11-02 10:20:17 -0600

    deploying site

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	let name = 'Svelte';
+	let svelte = 'Way cooler way better'
+	let vroom = "i'm in me mums car"
+</script>
+<h1 class="text-yellow-500">Hello {name}!</h1>
+<h1>Hello {name.toUpperCase()}!</h1>
+<h2>{svelte}</h2>
+<p>{vroom}</p>
